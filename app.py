7f16c0d52a68a6ed242a7790e75f5cc1c1c0863c
ruk <ruk.lakhani@students.makeschool.com>
2019-12-10 13:55:17 -0800

    2nd commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is Cool!!')
+
 if __name__ == '__main__':
     app.run(debug=True)

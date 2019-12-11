ceb72326d12513b0f39644f8e6dcb6a696c6515c
ruk <ruk.lakhani@students.makeschool.com>
2019-12-11 09:57:42 -0800

    users can destroy playlists

--- app.py
+++ app.py
@@ -34,12 +34,12 @@ def playlists_index():
 
 @app.route('/playlists/new')
 def playlists_new():
-    """create a new playlist"""
+    """create a new playlist."""
     return render_template('playlists_new.html')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-    """Submit a new playlist."""
+    """submit a new playlist."""
     # Grab the video IDs and make a list out of them
     video_ids = request.form.get('video_ids').split()
     # call our helper function to create the list of links
@@ -55,9 +55,41 @@ def playlists_submit():
 
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
-    """Show a single playlist."""
+    """show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    # Add the title parameter here
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)

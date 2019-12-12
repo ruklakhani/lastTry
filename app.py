a559c0f70bab1561368760273f57e5d2a415748c
ruk <ruk.lakhani@students.makeschool.com>
2019-12-11 22:10:39 -0800

    pushing to heroku

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
+import os
 
-
-client = MongoClient()
-db = client.Playlister
-playlists = db.playlists
 app = Flask(__name__)
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+playlists = db.playlists
+comments = db.comments
 
 def video_url_creator(id_lst):
     videos = []
@@ -98,4 +104,4 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
